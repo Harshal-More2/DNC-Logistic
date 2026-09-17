016213e29f2b6ea815863dd1468a95bb15b4582e
Harshal More <Harshal.hm207@gmail.com>
2026-09-17 09:54:28 +0530

    Fix component import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 import ScrollToTop from "./components/ScrollToTop";
 
 import Home from "./pages/Home";
